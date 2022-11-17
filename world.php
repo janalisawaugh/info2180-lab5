e0ddfbb3022b2c5cee2f1a09d47284af7d1f3249
Janalisa Waugh <iamjanalisa@gmail.com>
2022-11-17 02:50:12 -0500

    Trying to figure out function for country search in exercise 3

--- world.php
+++ world.php
@@ -4,12 +4,27 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-
+#
+$pg = $_SERVER['REQUEST_URI'];
+$url = parse_url($pg, PHP_URL_QUERY);
+parse_str($url, $parameter);
+$context = $parameter['context'];
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
 $stmt = $conn->query("SELECT * FROM countries");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$countryQuery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+$countryResults =  $countryQuery->fetchAll(PDO::FETCH_ASSOC);
+
+if($context=="countries"){
+  foreach ($countryResults as $row)
+  {
+    echo '<tr><td>' . $row['name'] . '<tr><td>' . $row['continent'] . '<tr><td>' . $row['independence_year'] .'<tr><td>' . $row['head_of_state'] . '<tr><td>';
+  }
+  
+  echo '</table>';
+  
+}
 
 ?>
 <ul>
